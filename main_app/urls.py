eb15ffa7e62d99857da991a75a92fe1d883040e5
parthishere <parthishere1234@gmail.com>
2021-04-23 00:04:47 +0530

    changes

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,16 @@
 
 
 from django.urls import path, include
-from .views import list_function, add_todo, detail_todo
+from .views import list_function, add_todo, detail_todo, edit_todo, archives_list, archive_todo
 
 app_name = 'main_app'
 
 urlpatterns = [
     path('', list_function, name='home'),
     path('create/', add_todo, name='create'),
-    path('<int:pk>', detail_todo, name='detail')
+    path('<int:pk>', detail_todo, name='detail'),
+    path('<int:pk>/edit', edit_todo, name='edit'),
+    path('<int:pk>/delete', edit_todo, name='delete'),
+    path('<int:pk>/archive', archive_todo, name='archive'),
+    path('archives', archives_list, name='archive-list'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .forms import TodoModelForm
 from .models import ToDoModel
 
@@ -24,7 +24,20 @@ def add_todo(request):
     
     return render(request, "main_app/add_todo.html", context=context)
             
+def edit_todo(request, pk=None):
+    instance = get_object_or_404(ToDoModel, pk=pk)
+    todo_form = TodoModelForm(request.POST or None, instance=instance)
+    context = {}
+    context['form'] = todo_form
+    if request.POST:
+        if todo_form.is_valid():
+            instance = todo_form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            context['form'] = todo_form
+            return redirect('main_app:home')
     
+    return render(request, "main_app/add_todo.html", context=context)   
 
 
 def detail_todo(request, pk=None):
@@ -41,3 +54,13 @@ def delete_list(request, pk=None):
 	}
 	
 	return render(request, 'main_app/home.html', context)
+
+
+def archives_list(request):
+    pass
+
+def archive_todo(request):
+    pass
+
+def search_todo(request):
+    pass
