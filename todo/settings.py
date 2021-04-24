4c46c625e2d003b6b4a231996e7a73b355c139b7
parthishere <parthishere1234@gmail.com>
2021-04-24 15:11:54 +0530

    chnages

--- todo/settings.py
+++ todo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from secrets import SK 
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SK
+SECRET_KEY = '%v1ohze)m+8#@5!fr@a8uf#5h38%a=)c2kw6=9^r-_(&j-2k=m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
