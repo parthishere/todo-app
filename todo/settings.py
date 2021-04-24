7d674fe7021aef48c4ad00c3233861d03d0aa0bd
parthishere <parthishere1234@gmail.com>
2021-04-24 21:10:34 +0530

    changes

--- todo/settings.py
+++ todo/settings.py
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('todo_app_secret_key')
+# SECRET_KEY = os.environ.get('todo_app_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
